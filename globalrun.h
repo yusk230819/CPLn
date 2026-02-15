2a28595f89232c7905f2a418763af229afccbcea
yusk230819 <yusk2906@gmail.com>
2026-02-15 17:47:00 +0900

    Update globalrun.h

--- globalrun.h
+++ globalrun.h
@@ -22,6 +22,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+include <stdio.h>
+
     static inline void put_pixel(CPLnEngine* eng, int x, int y, uint32_t color){
     if (x < 0 || y < 0 ||
         x >= eng->fb.width ||
